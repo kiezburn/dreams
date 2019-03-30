bef85d318ef51b2835fa1b3c64c8e90e56ba34a7
edward mccaughan <edwardmccaughan@gmail.com>
2019-03-30 15:13:43 +0100

    more production config fiddling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
   }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => ENV['DOMAIN_NAME'] }
-  config.action_mailer.asset_host = ENV['DOMAIN_NAME']
+  config.action_mailer.default_url_options = { :host => ENV['DOMAIN'] }
+  config.action_mailer.asset_host = ENV['DOMAIN']
 
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
