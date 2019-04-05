deb7e8b820d55b9a1d222b9265e9b61410759c06
hesslau <henrik@hesslau.org>
2019-04-05 12:04:06 +0200

    set from name and subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def registration_invite(ticket)
     @ticket = ticket
-    mail(to: @ticket.email, subject: 'Register for dreams')
+    mail(to: @ticket.email, subject: ENV['EMAIL_FROM_NAME'])
   end
 end
